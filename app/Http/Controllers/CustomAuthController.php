c0896bb527d26bb9c9ad6d3d159a84ef5290af39
Aclaputra <muhamadacla22@gmail.com>
2022-12-28 07:30:58 +0700

    feat: modified custom auth controller

--- app/Http/Controllers/CustomAuthController.php
+++ app/Http/Controllers/CustomAuthController.php
@@ -3,45 +3,52 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Contracts\Session\Session;
+use Session;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Redirect;
 
-class CustomAuthController extends Controller
-{
+class CustomAuthController extends Controller {
+    // views
+    // registration used only on admin dashboard
+    public function reqistration() {return view('admin.registration');}
+    public function login() {return view('login');}
 
-  /**
-   * functions
-   */
-  // registration used only on admin dashboard
-  public function reqistration() {return view('admin.registration');}
-  public function login() {return view('login');}
-  public function customLogin(Request $request) {
-    $credentials = $request->only('email', 'password');
-    $request->validate(['email' => 'required','password' => 'required',]);
-    if (Auth::attempt($credentials)) {
-      return redirect()->intended('dashboard')->withSuccess('Signed in');
+    // register and login function
+    public function customLogin(Request $request) {
+        $credentials = $request->only('email', 'password');
+        $request->validate(['email' => 'required','password' => 'required',]);
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('dashboard')->withSuccess('Signed in');
+        }
+        return redirect("login")->withSuccess("Login details are not valid");
     }
-    return redirect("login")->withSuccess("Login details are not valid");
-  }
-  public function create(array $data) {
-    return User::create([
-      'name' => $data['name'],
-      'email' => $data['email'],
-      'password' => Hash::make($data['password']),
-      'is_admin' => 0
-    ]);
-  }
-  public function dashboard() {
-    if (Auth::check()) {
-      return view('dashboard');
+    public function customRegistration(Request $request) {
+        $request->validate([
+          'name' => 'required',
+          'email' => 'required|email|unique:users',
+          'password' => 'required|min:6'
+        ]);
+        $data = $request->all();
+        $check = $this->create($data);
+        return redirect("dashboard")->withSuccess("You have signed-in");
     }
-    // #implementThisLater
-    // if is_admin = 1 redirect admin.dashboard else is_admin = 0 redirect
-    // dashboard mahasiswa
-    return redirect("login")->withSuccess("You are not allowed to access");
-  }
-  public function signOut() {Session::flush();Auth::logout();return Redirect('login');}
+
+    // create item new user function
+    public function create(array $data) {
+        return User::create([
+          'name' => $data['name'],
+          'email' => $data['email'],
+          'password' => Hash::make($data['password']),
+          'is_admin' => 0
+        ]);
+    }
+    public function dashboard() {
+        if (Auth::check()) { return view('dashboard'); }
+        // #implementThisLater
+        // if is_admin = 1 redirect admin.dashboard else is_admin = 0 redirect
+        // dashboard mahasiswa
+        return redirect("login")->withSuccess("You are not allowed to access");
+    }
+    public function signOut() {Session::flush();Auth::logout();return Redirect('login');}
 }
