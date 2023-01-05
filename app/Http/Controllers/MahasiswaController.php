6e494176bb2390a6cbdb1e9e0c5d2a8c3ecd3597
Aclaputra <muhamadacla22@gmail.com>
2023-01-06 06:33:36 +0700

    feat: added mahasiswa controller

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -2,22 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\DataTables\UsersDataTable;
+//use App\Models\User;
+//use Illuminate\Http\Request;
 
 class MahasiswaController extends Controller
 {
-    public function index(Request $request) {
-        if ($request->ajax()) {
-            $data = User::select('name','email')->get();
-            return Datatables::of($data)->addIndexColumn()
-                ->addColumn('action', function($row){
-                    $btn = '<a href="javascript:void(0)" class="btn btn-primary btn-sm">View</a>';
-                    return $btn;
-                })
-                ->rawColumns(['action'])
-                ->make(true);
-        }
-
+    public function index()
+    {
         return view('dashboard');
+    }
+    public function getMahasiswa(UsersDataTable $dataTable)
+    {
+
+        return $dataTable->render('dashboard');
     }
 }
