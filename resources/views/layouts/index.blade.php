d56579d531ab6dccac7830b75bd746c48d21c06e
Aclaputra <muhamadacla22@gmail.com>
2023-01-06 06:32:17 +0700

    feat: added layouts index blade

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -3,8 +3,9 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>SPP management</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
@@ -34,22 +35,31 @@
       </div>
     </nav>
     @yield('content')
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
   </body>
+  <script src="{{ asset('vendor/datatables/buttons.server-side.js') }}"></script>
+  @stack('scripts')
+<!--
   <script type="text/javascript">
-      $(function () {
+      $(document).ready(function () {
           var table = $('.user_datatable').DataTable({
               processing: true,
               serverSide: true,
               ajax: "{{ route('mahasiswa.dashboard') }}",
               columns: [
+                  {data: 'DT_Row_Index', name:'DT_Row_Index' },
                   {data: 'id', name: 'id'},
                   {data: 'name', name: 'name'},
                   {data: 'email', name: 'email'},
-                  {data: 'action', name: 'action', orderable: false, searchable: false},
+                  {
+                      data: 'action',
+                      name: 'action',
+                      orderable: true,
+                      searchable: true
+                  },
               ]
           });
       });
+-->
   </script>
-
 </html>
