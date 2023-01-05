19ca0a2c2d85996b2cc6f588a31a1094bf560a95
Aclaputra <muhamadacla22@gmail.com>
2023-01-06 06:33:05 +0700

    feat: added dashboard blade

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,20 +3,25 @@
 @section('content')
   <h1>dashboard mahasiswa</h1>
   <div class="container">
-      <div class="row">
-          <div class="col-12 table-responsive">
-              <table class="table table-bordered user_datatable">
-                  <thead>
-                  <tr>
-                      <th>ID</th>
-                      <th>Name</th>
-                      <th>Email</th>
-                      <th width="100px">Action</th>
-                  </tr>
-                  </thead>
-                  <tbody></tbody>
-              </table>
-          </div>
-      </div>
+{{--      <div class="row">--}}
+{{--          <div class="col-12 table-responsive">--}}
+{{--              <table class="table table-bordered user_datatable">--}}
+{{--                  <thead>--}}
+{{--                  <tr>--}}
+{{--                      <th>ID</th>--}}
+{{--                      <th>Name</th>--}}
+{{--                      <th>Email</th>--}}
+{{--                      <th width="100px">Action</th>--}}
+{{--                  </tr>--}}
+{{--                  </thead>--}}
+{{--                  <tbody></tbody>--}}
+{{--              </table>--}}
+{{--          </div>--}}
+{{--      </div>--}}
+      {{$dataTable->table()}}
   </div>
-@stop
+@endsection
+
+@push('scripts')
+    {{$dataTable->scripts()}}
+@endpush
